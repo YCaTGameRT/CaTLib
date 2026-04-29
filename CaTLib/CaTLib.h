78c188b9af3e5aa6236ff7203a55822ebc097cc9
YCaTGameRT <ycatgamert@gmail.com>
2026-04-30 00:02:51 +0500

    header complete

--- CaTLib/CaTLib.h
+++ CaTLib/CaTLib.h
@@ -3,9 +3,18 @@
 
 #include <stdio.h>
 #include <locale.h>
+#include <vector>
+#include <string>
+#include <map>
 
 class CaTLib {
-	
+public:
+	void Print();
+	void AddElement(int num, std::string desc);
+private:
+	std::string title;
+	std::map<int, std::string> element;
+	std::vector<std::map<int, std::string>> allElements;
 };
 
 #endif
