32143d6c1087ef0dc380613ffbfd92adfa46ac78
YCaTGameRT <ycatgamert@gmail.com>
2026-05-07 13:24:29 +0500

    убраны конструкторы

--- CaTLibTest26/CaTLibTest26.cpp
+++ CaTLibTest26/CaTLibTest26.cpp
@@ -7,15 +7,6 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace CaTLibTest26 {
 	TEST_CLASS(CaTLibTest26) {
 	public:
-		TEST_METHOD(VoidConstructorTest) {
-			CaTLib lib;
-			Assert::IsTrue(lib.GetTitle() == "");
-		}
-		TEST_METHOD(ConstructorTest) {
-			CaTLib lib("тест");
-			Assert::IsTrue(lib.GetTitle() != "");
-		}
-
 		TEST_METHOD(GetTest) {
 			CaTLib lib("тест");
 			lib.AddElement(1, "1");
