72710fc02624b1622ec1741f1877e806e98caf21
none <none@gmail.com>
2026-05-07 13:01:21 +0500

    перенос на VS26

--- CaTLibTest26/CaTLibTest26.cpp
+++ CaTLibTest26/CaTLibTest26.cpp
@@ -1,21 +1,19 @@
-#include "stdafx.h"
+#include "pch.h"
 #include "CppUnitTest.h"
-#include "../CaTLib/CaTLib.h"
+#include "../CaTLib26/CaTLib.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace CaTLibTests {		
-	TEST_CLASS(UnitTest1) {
+namespace CaTLibTest26 {
+	TEST_CLASS(CaTLibTest26) {
 	public:
 		TEST_METHOD(VoidConstructorTest) {
 			CaTLib lib;
-			Assert::IsTrue(lib.GetTitle == "");
+			Assert::IsTrue(lib.GetTitle() == "");
 		}
 		TEST_METHOD(ConstructorTest) {
 			CaTLib lib("тест");
 			Assert::IsTrue(lib.GetTitle() != "");
 		}
-
-
 	};
 }
