6d3f36e0afbf6d53d63296318f469e76a8147169
YCaTGameRT <ycatgamert@gmail.com>
2026-05-07 05:28:56 +0500

    план тестов

--- CaTLibTests/unittest1.cpp
+++ CaTLibTests/unittest1.cpp
@@ -1,15 +1,17 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "../CaTLib/CaTLib.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace CaTLibTests {		
 	TEST_CLASS(UnitTest1) {
 	public:
-		TEST_METHOD(TestMethod1) {
+		TEST_METHOD(ConstructorTest) {
 			// TODO: –азместите здесь код своего теста
+			CaTLib lib("тестирование");
 		}
-		TEST_METHOD(TestMethod1) {
+		TEST_METHOD(TestMethod2) {
 			// TODO: –азместите здесь код своего теста
 		}
 	};
